3a927f6d3f0997f5d2bba7d1a47eccb5b75f0594
Hoang Long <dphlong2711@gmail.com>
2026-04-10 18:34:27 +0700

    replace

--- BE/routes/web.php
+++ BE/routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Giao diện test API
-Route::get('/docs', function () {
-    return view('api-docs');
-});
